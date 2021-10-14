bee0b503d04666d5fef9d85795c9a814fb414513
iamfrego <mate_freg@tiscali.it>
2021-10-14 16:16:53 +0200

    js done

--- EsEmail/assets/js/main.js
+++ EsEmail/assets/js/main.js
@@ -1,1 +1,29 @@
-alert("Link");
+const checkButtonElement = document.getElementById('accedi');
+let labelPrint = document.getElementById("check");
+
+const loggati = ["a@a.it", "b@b.it", "c@c.it"];
+console.log(loggati);
+
+checkButtonElement.addEventListener(`click`, function () {
+
+    const emailElement = document.getElementById(`input_email`);
+    const emailValue = emailElement.value;
+
+    const checkMessage = "Bentornato";
+    const notCheckMessage = "Utente non registrato";
+
+    console.log(emailValue);
+
+    for (let i = 0; i < loggati.length; i++) {
+        if (emailValue === loggati[i]) {
+            labelPrint.innerHTML = checkMessage;
+            console.log(labelPrint);
+            return 0;
+
+        } else {
+            labelPrint.innerHTML = notCheckMessage;
+        }
+
+
+    }
+})
