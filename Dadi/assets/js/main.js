baa8c212b1b6be6886af9d31e1a4b84b1d1ed316
iamfrego <mate_freg@tiscali.it>
2021-10-14 16:55:53 +0200

    Error display number

--- Dadi/assets/js/main.js
+++ Dadi/assets/js/main.js
@@ -1,1 +1,16 @@
-alert("Link");
+const userNumber = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+console.log(userNumber);
+
+const pcNumber = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+console.log(pcNumber);
+
+
+let labelPrint = document.getElementById("winner");
+
+if (userNumber > pcNumber) {
+    labelPrint.innerHTML = `Il vincitore sei tu!`;
+} else if (pcNumber > userNumber) {
+    labelPrint.innerHTML = `Il vincitore è il PC!`;
+} else {
+    labelPrint.innerHTML = `PARITà!`;
+}
